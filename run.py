010f76f9aa8a545df89321130d74d91ccb0b8a7e
revdoor <revdoor@kaist.ac.kr>
2022-06-21 16:40:30 +0900

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import discord
 import os
 from discord.ext import commands
 import json
+import youtube_dl
 
 
 def command_match(text, command_data):
@@ -21,7 +22,7 @@ def command_match(text, command_data):
 
 try:
     game = discord.Game("로아 이모티콘")
-    bot = commands.Bot(command_prefix="[", status=discord.Status.online, activity=game)
+    bot = commands.Bot(command_prefix="~~", status=discord.Status.online, activity=game)
     
     with open("command.json", "r") as json_file:
         command_data = json.load(json_file)
@@ -51,6 +52,51 @@ try:
     
     async def help_message(ctx):
         await ctx.send(content=", ".join(command_data.keys()))
+    
+    @bot.command()
+    async def play(ctx):
+        channel = ctx.author.voice.channel
+        await channel.connect()
+    
+    @bot.command()
+    async def play(ctx, url):
+        channel = ctx.author.voice.channel
+        if bot.voice_clients == []:
+            await channel.connect()
+            await ctx.send("connected to the voice channel, " + str(bot.voice_clients[0].channel))
+    
+        ydl_opts = {'format': 'bestaudio'}
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            URL = info['formats'][0]['url']
+        voice = bot.voice_clients[0]
+        voice.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+    
+    @bot.command()
+    async def pause(ctx):
+        if not bot.voice_clients[0].is_paused():
+            bot.voice_clients[0].pause()
+        else:
+            await ctx.send("already paused")
+    
+    @bot.command()
+    async def resume(ctx):
+        if bot.voice_clients[0].is_paused():
+            bot.voice_clients[0].resume()
+        else:
+            await ctx.send("already playing")
+    
+    @bot.command()
+    async def stop(ctx):
+        if bot.voice_clients[0].is_playing():
+            bot.voice_clients[0].stop()
+        else:
+            await ctx.send("not playing")    
+    
+    @bot.command()
+    async def leave(ctx):
+        await bot.voice_clients[0].disconnect()
     
     bot.run(os.environ['token'])
 except Exception as e:
