95fcaee0f61ea8c74835da8b0157b515f97f86d7
revdoor <revdoor@kaist.ac.kr>
2022-06-26 17:07:23 +0900

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import os
 from discord.ext import commands
 import json
 import youtube_dl
+import random
 
 
 def command_match(text, command_data):
@@ -58,19 +59,31 @@ try:
         await ctx.send(content=", ".join(command_data.keys()))
     
     @bot.command()
-    async def play(ctx, url):
+    async def join(ctx):
+        if not ctx.author.voice:
+            await ctx.send("사용자가 음성 채널에 연결되어 있지 않습니다")
+            return
+        
         channel = ctx.author.voice.channel
-        if bot.voice_clients == []:
-            await channel.connect()
-            await ctx.send("{} 음성 채널에 연결하였습니다".format(str(bot.voice_clients[0].channel)))
+        bot.queue = {}
+        await ctx.send(f"{channel} 음성 채널에 연결하였습니다")
+        await channel.connect()
     
-        ydl_opts = {'format': 'bestaudio'}
-        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=False)
-            URL = info['formats'][0]['url']
-        voice = bot.voice_clients[0]
-        voice.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+    @bot.command()
+    async def play(ctx, url):
+        if bot.voice_clients == []:
+            await bot.join(ctx)
+        
+        try:
+            ydl_opts = {'format': 'bestaudio'}
+            FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(url, download=False)
+                URL = info['formats'][0]['url']
+            voice = bot.voice_clients[0]
+            voice.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+        except:
+            await ctx.send("문제가 발생하였습니다. 다시 시도해주세요")
     
     @bot.command()
     async def pause(ctx):
@@ -84,18 +97,27 @@ try:
         if bot.voice_clients[0].is_paused():
             bot.voice_clients[0].resume()
         else:
-            await ctx.send("이미 재생중입니다")
+            await ctx.send("이미 재생 중입니다")
     
     @bot.command()
-    async def stop(ctx):
+    async def skip(ctx):
         if bot.voice_clients[0].is_playing():
             bot.voice_clients[0].stop()
         else:
-            await ctx.send("재생중이 아닙니다")
+            await ctx.send("재생 중이 아닙니다")
     
     @bot.command()
     async def leave(ctx):
         await bot.voice_clients[0].disconnect()
+        await ctx.send("봇 연결이 해제되었습니다")
+    
+    @bot.command(aliases=["뽑기"])
+    async def draw(ctx, draw_no, *names):
+        try:
+            draw_no = int(draw_no)
+            
+            await ctx.send("{}에서 {}개를 뽑습니다.\n뽑은 값들은 {}입니다.".format(names, draw_no,
+                                                                                   " ".join([random.choice(names) for _ in range(draw_no)])))
     
     bot.run(os.environ['token'])
 except Exception as e:
