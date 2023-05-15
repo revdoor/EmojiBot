089da29746842955647d8a230c93c0f5a91d9051
revdoor <revdoor@kaist.ac.kr>
2023-05-15 23:37:17 +0900

    FEATURE - use google spreadsheet for command
    ### DESC
    - put command - image url in spreadsheet
    - use them instead of local storage

--- run.py
+++ run.py
@@ -7,73 +7,81 @@ import json
 import youtube_dl
 import random
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-def command_match(text, command_data):
-    for key in command_data.keys():
-        length = len(key)
-        
-        if len(text) != length:
-            continue
-        
-        if text == key:
-            return True, key
+
+scope = [
+'https://spreadsheets.google.com/feeds',
+'https://www.googleapis.com/auth/drive',
+]
+credentials = ServiceAccountCredentials.from_json(os.environ['json'], scope)
+gc = gspread.authorize(credentials)
+spreadsheet_url = os.environ['spreadsheet']
+doc = gc.open_by_url(spreadsheet_url)
+
+
+def get_command_info():
+    global command_data
     
-    return False, None
+    command_no = int(doc.worksheet('number').acell('A1').value)
+    
+    command_raw = doc.worksheet('command').range(f'A1:B{command_no}')
+    
+    for i in range(command_no):
+        name = command_raw[2*i].value
+        url = command_raw[2*i+1].value
+        
+        command_data[name] = url
 
 
 try:
     game = discord.Game("로아 이모티콘")
     bot = commands.Bot(command_prefix="~~", status=discord.Status.online, activity=game)
     
-    with open("command.json", "r") as json_file:
-        command_data = json.load(json_file)
-    
+    command_data = dict()
+
+    get_command_info()
+
     @bot.event
     async def on_ready():
         print("Starting bot")
-    
+
     @bot.event
     async def on_message(message):
-        text = message.content
-        
-        res, key = command_match(text, command_data)
-        
-        if res:
-            ctx = await bot.get_context(message)
-            await send_image(ctx, command_data[key])
+        text = message.contentr
+
+        if text in command_data:
+            await ctx.channel.purge(limit=1, check=lambda m: m.author == ctx.author)
+            await ctx.send(command_data[text])
             return
-        
+
         if text == "[도움말]" or text == "<도움말>":
             ctx = await bot.get_context(message)
             await help_message(ctx)
             return
-        
+
         await bot.process_commands(message)
-    
-    async def send_image(ctx, img_name):
-        await ctx.channel.purge(limit=1, check=lambda m: m.author == ctx.author)
-        await ctx.send(content=f"{ctx.author.nick if ctx.author.nick else ctx.author.name}:",
-                       file=discord.File("emoji/"+img_name))
-    
+
     async def help_message(ctx):
         await ctx.send(content=", ".join(command_data.keys()))
-    
+
     @bot.command()
     async def join(ctx):
         if not ctx.author.voice:
             await ctx.send("사용자가 음성 채널에 연결되어 있지 않습니다")
             return
-        
+
         channel = ctx.author.voice.channel
         bot.queue = {}
         await ctx.send(f"{channel} 음성 채널에 연결하였습니다")
         await channel.connect()
-    
+
     @bot.command()
     async def play(ctx, url):
         if bot.voice_clients == []:
             await bot.join(ctx)
-        
+
         try:
             ydl_opts = {'format': 'bestaudio'}
             FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
@@ -84,33 +92,33 @@ try:
             voice.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
         except:
             await ctx.send("문제가 발생하였습니다. 다시 시도해주세요")
-    
+
     @bot.command()
     async def pause(ctx):
         if not bot.voice_clients[0].is_paused():
             bot.voice_clients[0].pause()
         else:
             await ctx.send("이미 일시정지된 상태입니다")
-    
+
     @bot.command()
     async def resume(ctx):
         if bot.voice_clients[0].is_paused():
             bot.voice_clients[0].resume()
         else:
             await ctx.send("이미 재생 중입니다")
-    
+
     @bot.command()
     async def skip(ctx):
         if bot.voice_clients[0].is_playing():
             bot.voice_clients[0].stop()
         else:
             await ctx.send("재생 중이 아닙니다")
-    
+
     @bot.command()
     async def leave(ctx):
         await bot.voice_clients[0].disconnect()
         await ctx.send("봇 연결이 해제되었습니다")
-    
+
     @bot.command(aliases=["뽑기"])
     async def draw(ctx, draw_no, *names):
         try:
@@ -121,7 +129,8 @@ try:
                                                                                         " ".join([random.choice(names) for _ in range(draw_no)])))
         except:
             pass
-    
+
     bot.run(os.environ['token'])
+
 except Exception as e:
     print(e)
